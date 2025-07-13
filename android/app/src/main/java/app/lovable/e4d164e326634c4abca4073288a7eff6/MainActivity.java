d4f10055bd815ffaa290dd08b4e87c17f475d436
Mohammad AbuMeshref <mshref17@gmail.com>
2025-07-13 19:45:34 +0300

    my first push

---
+++ android/app/src/main/java/app/lovable/e4d164e326634c4abca4073288a7eff6/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.e4d164e326634c4abca4073288a7eff6;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,7 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.e4d164e326634c4abca4073288a7eff6',
   appName: 'mobile-app-genie-touch',
   webDir: 'dist',
+  bundledWebRuntime: false,
   server: {
     url: 'https://e4d164e3-2663-4c4a-bca4-073288a7eff6.lovableproject.com?forceHideBadge=true',
     cleartext: true
